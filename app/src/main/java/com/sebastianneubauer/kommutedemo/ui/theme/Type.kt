deeb6a559be2f1de4b6b3a9cd2040e840bac6a53
snappdevelopment <se.neu@web.de>
2022-11-21 06:59:08 +0100

    Minimize api surface

--- app/src/main/java/com/sebastianneubauer/kommutedemo/ui/theme/Type.kt
+++ app/src/main/java/com/sebastianneubauer/kommutedemo/ui/theme/Type.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
-val Typography = Typography(
+internal val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
